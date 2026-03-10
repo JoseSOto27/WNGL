edd9f9c063d5d82ac24c8304c3087d51f5fcb3f2
JoseSOto27 <snowball@MacBook-Air-de-Jose.local>
2026-03-09 18:49:14 -0600

    Fix: Enviando puntosUsados al servidor de pagos

--- sistema/src/Components/Common/verificarHorario.js
+++ sistema/src/Components/Common/verificarHorario.js
@@ -7,7 +7,7 @@ export const checkWingoolStatus = () => {
   const config = {
     abierto: 13.0, // 1:00 PM
     cierre: 22.0,   // 10:00 PM
-    esLunes: dia === 3
+    esLunes: dia === 1
   };
 
   let status = "ABIERTO";
